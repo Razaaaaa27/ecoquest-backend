6e53df72dbb78582050dd550963a4479aea90949
Muhammad Raza Adzani <raza.a22@mhs.usk.ac.id>
2025-06-03 15:53:02 +0700

    refactor: centralize model exports through index.js

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const User = require('./User');
+const Challenge = require('./Challenge');
+const Forest = require('./forest');
+const Post = require('./post');
+const EduContent = require('./EduContent');
+const Submission = require('./Submission');
+const ChallengeSubmission = require('./challengeSubmission');
+
+module.exports = {
+  User,
+  Challenge,
+  Forest,
+  Post,
+  EduContent,
+  Submission,
+  ChallengeSubmission
+}; 
